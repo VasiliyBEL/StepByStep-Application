cc232aac71f0621999ba1b699fa4382b98b26e89
VasiliyBEL <vaspn001@gmail.com>
2022-08-29 20:30:19 +0300

    Build a structure of the App

---
+++ StepByStep Application/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepByStep_Application.Models
+{
+    internal class Users
+    {
+        public int Rank { get; set; }
+
+        public string? User { get; set; }
+
+        public string? Status { get; set; }
+
+        public int Steps { get; set; }
+    }
+}
